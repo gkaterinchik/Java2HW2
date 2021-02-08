7c08069561939bc1017e598a0d44ca712de54ff0
gkaterinchik <62135154+gkaterinchik@users.noreply.github.com>
2021-02-08 15:17:58 +0300

    ДЗ урок 5 Многопоточность

--- src/Lesson5/Multithreading.java
+++ src/Lesson5/Multithreading.java
@@ -29,6 +29,9 @@ public class Multithreading {
 
         @Override
         public void run() {
+            for (int i = 0; i < h; i++){
+                values[i] = 1;
+            }
             for (int i = 0; i < h; i++) {
                 values[i] = (float) (values[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
 
